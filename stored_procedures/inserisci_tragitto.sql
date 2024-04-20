b7e0847bf7a17ecff273df0e5963d3b2f4a2b21a
Carmine Colucci <carminecolucci22@gmail.com>
2024-04-20 18:49:01 +0200

    bugfix

--- stored_procedures/inserisci_tragitto.sql
+++ stored_procedures/inserisci_tragitto.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE PROCEDURE INSERISCI_TRAGITTO(
 ) AS
 BEGIN
 	DECLARE
-		id TRAGITTI.id%TYPE := tragitti_id_sequence.nextval,
+		id TRAGITTI.id%TYPE := tragitti_id_sequence.nextval;
 	BEGIN
 		INSERT INTO TRAGITTI VALUES (id, dispositivo, ingresso, data_ingresso, uscita, data_uscita);
 		COMMIT;
